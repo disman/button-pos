84c7dac9e3b991124054f307eb56c08f60952c30
disman <disman810@gmail.com>
2020-05-18 23:31:16 +0700

    Membuat helper login dan library login

---
+++ application/helpers/login_helper.php
@@ -0,0 +1,19 @@
+<?php
+
+function already_login()
+{
+    $ci = &get_instance();
+    $user_session = $ci->session->userdata('user_id');
+    if ($user_session) {
+        redirect('dashboard');
+    }
+}
+
+function not_login()
+{
+    $ci = &get_instance();
+    $user_session = $ci->session->userdata('user_id');
+    if (!$user_session) {
+        redirect('auth');
+    }
+}
